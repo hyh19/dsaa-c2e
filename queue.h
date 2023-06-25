0cefb3959b7db1d773821355fe16abdc7abe7f6e
e7e26e5 <abc@xyz.com>
2023-06-25 11:44:07 +0800

    refactor

--- queue.h
+++ queue.h
@@ -1,20 +1,27 @@
-        typedef int ElementType;
-/* START: fig3_57.txt */
-        #ifndef _Queue_h
-        #define _Queue_h
+typedef int ElementType;
 
-        struct QueueRecord;
-        typedef struct QueueRecord *Queue;
+#ifndef _Queue_h
+#define _Queue_h
 
-        int IsEmpty( Queue Q );
-        int IsFull( Queue Q );
-        Queue CreateQueue( int MaxElements );
-        void DisposeQueue( Queue Q );
-        void MakeEmpty( Queue Q );
-        void Enqueue( ElementType X, Queue Q );
-        ElementType Front( Queue Q );
-        void Dequeue( Queue Q );
-        ElementType FrontAndDequeue( Queue Q );
+struct QueueRecord;
+typedef struct QueueRecord *Queue;
 
-        #endif  /* _Queue_h */
-/* END */
+int IsEmpty(Queue Q);
+
+int IsFull(Queue Q);
+
+Queue CreateQueue(int MaxElements);
+
+void DisposeQueue(Queue Q);
+
+void MakeEmpty(Queue Q);
+
+void Enqueue(ElementType X, Queue Q);
+
+ElementType Front(Queue Q);
+
+void Dequeue(Queue Q);
+
+ElementType FrontAndDequeue(Queue Q);
+
+#endif  /* _Queue_h */
