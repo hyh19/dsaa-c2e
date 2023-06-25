5af7bf43f610634bd9502565b0452e8c15f87547
e7e26e5 <abc@xyz.com>
2023-06-25 11:25:37 +0800

    refactor

--- stackli.h
+++ stackli.h
@@ -1,20 +1,24 @@
-        typedef int ElementType;
-/* START: fig3_39.txt */
-        #ifndef _Stack_h
-        #define _Stack_h
+typedef int ElementType;
 
-        struct Node;
-        typedef struct Node *PtrToNode;
-        typedef PtrToNode Stack;
+#ifndef _Stack_h
+#define _Stack_h
 
-        int IsEmpty( Stack S );
-        Stack CreateStack( void );
-        void DisposeStack( Stack S );
-        void MakeEmpty( Stack S );
-        void Push( ElementType X, Stack S );
-        ElementType Top( Stack S );
-        void Pop( Stack S );
+struct Node;
+typedef struct Node *PtrToNode;
+typedef PtrToNode Stack;
 
-        #endif  /* _Stack_h */
+int IsEmpty(Stack S);
 
-/* END */
+Stack CreateStack(void);
+
+void DisposeStack(Stack S);
+
+void MakeEmpty(Stack S);
+
+void Push(ElementType X, Stack S);
+
+ElementType Top(Stack S);
+
+void Pop(Stack S);
+
+#endif  /* _Stack_h */
