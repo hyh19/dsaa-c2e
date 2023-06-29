c46642addb4a087ec3ba07c059dea4d820bc016f
e7e26e5 <abc@xyz.com>
2023-06-29 10:39:22 +0800

    refactor

--- list.h
+++ list.h
@@ -1,6 +1,5 @@
 typedef int ElementType;
 
-/* START: fig3_6.txt */
 #ifndef _List_H
 #define _List_H
 
@@ -34,4 +33,3 @@ Position Advance(Position P);
 ElementType Retrieve(Position P);
 
 #endif    /* _List_H */
-/* END */
--- testlist.c
+++ testlist.c
@@ -5,9 +5,9 @@ void
 PrintList(const List L) {
     Position P = Header(L);
 
-    if (IsEmpty(L))
+    if (IsEmpty(L)) {
         printf("Empty list\n");
-    else {
+    } else {
         do {
             P = Advance(P);
             printf("%d ", Retrieve(P));
@@ -25,17 +25,21 @@ int main() {
     P = Header(L);
     PrintList(L);
 
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < 10; ++i) {
         Insert(i, L, P);
         PrintList(L);
         P = Advance(P);
     }
-    for (i = 0; i < 10; i += 2)
-        Delete(i, L);
 
-    for (i = 0; i < 10; i++)
-        if ((i % 2 == 0) == (Find(i, L) != NULL))
+    for (i = 0; i < 10; i += 2) {
+        Delete(i, L);
+    }
+
+    for (i = 0; i < 10; ++i) {
+        if ((i % 2 == 0) == (Find(i, L) != NULL)) {
             printf("Find fails\n");
+        }
+    }
 
     printf("Finished deletions\n");
 
